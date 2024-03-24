b0ce44f6a2ba7aef25671e4ac77c3a92ca5eeac0
Franklin <franklin.op@hotmail.com>
2024-03-24 18:31:05 +0100

    nest Sign in route on naviga1ion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import {Routes, Route} from 'react-router-dom'
 
-import Navigation from './routes/navigation/navigation.component'
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
-import Home from './routes/home/home.component'
 
 
 
@@ -19,6 +20,7 @@ const App = () => {
     <Route path='/' element={<Navigation/>} >
     <Route index element={<Home/>} />
     <Route path='shop' element = {<Shop /> } />
+    <Route path='signIn' element = {<SignIn /> } />
     </Route>
   </Routes>
   )
