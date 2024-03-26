3c252143651884f44512b7c2e5ce2c51cc972d60
Franklin <franklin.op@hotmail.com>
2024-03-26 13:25:36 +0100

    created sigin form component

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,6 +4,8 @@ import {
       createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
 
 const Authentication = ()=> {
 
@@ -16,7 +18,7 @@ const Authentication = ()=> {
     return (
         <div>
         <h1>Sign In Page </h1>
-        <button onClick={logGoogleUser} > Sign in with Google Popup </button>
+        <SignInForm/>
         <SignUpForm/>
   
         </div>
