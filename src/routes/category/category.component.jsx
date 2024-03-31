85209eb6f66cc04ddd686c82800b3f0ca3c99d9f
Franklin <franklin.op@hotmail.com>
2024-03-31 21:21:15 +0200

    updated categories display

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 
 import ProductCard from "../../components/product-card/product-card.component";
@@ -12,10 +12,12 @@ const Category = ()=> {
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(()=> {
-    setProducts(categoriesMap [category]);
-    },[category, categoriesMap])
+    setProducts(categoriesMap[category]);
+    }, [category, categoriesMap]);
 
     return (
+        <Fragment>
+        <h2 className="category-title"> {category.toUpperCase()} </h2>
         <div className="category-container">
             {
                products &&
@@ -24,6 +26,7 @@ const Category = ()=> {
                ))
             }
         </div>
+        </Fragment>
     )
 }
 
